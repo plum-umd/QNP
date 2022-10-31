d18be8aab8ab52ac953be943313b0f4232297bf9
liyili2@illinois.edu <liyili2@illinois.edu>
2022-10-30 22:51:46 -0400

    update proof system definition.

--- SessionProof.v
+++ SessionProof.v
@@ -46,7 +46,7 @@ Inductive sval := ST (x:state_elem) | SV (s:session)
                | SSL (e:sval) (a:sval) (b:bexp) (l1:session) (l2:session).
 *)
 
-Inductive sval := SV (s:session) | Frozen (b:bexp) (s:sval) | Unfrozen (b:bexp) (s:sval) | FM (x:var) (n:nat) (s:sval).
+Inductive sval := SV (s:session) | Frozen (b:bexp) (s:sval) | Unfrozen (n:nat) (b:bexp) (s:sval) | FM (x:var) (n:nat) (s:sval).
 
 Inductive cpred_elem := PFalse | CP (b:cbexp) | SEq (x:sval) (y:state_elem) | CNeg (b:cbexp).
                              (* x = y|_{u=z} x is a new session from the old y by cutting session u to be value z. *)
@@ -58,7 +58,7 @@ Definition fresh (l:nat) := l +1.
 Fixpoint sval_subst_c t x v :=
   match t with SV s => SV (subst_session s x v)
               | Frozen b s => Frozen (subst_bexp b x v) (sval_subst_c s x v)
-              | Unfrozen b s => Frozen (subst_bexp b x v) (sval_subst_c s x v)
+              | Unfrozen n b s => Unfrozen n (subst_bexp b x v) (sval_subst_c s x v)
               | FM y n s => FM y n (sval_subst_c s x v)
   end.
 
@@ -107,7 +107,7 @@ Fixpoint freeVarsBexp (a:bexp) := match a with CB b => (freeVarsCBexp b)
 
 Fixpoint freeSesSV (a:sval) := match a with SV s => [s]
          | Frozen b s => freeSesSV s
-         | Unfrozen b s => freeSesSV s
+         | Unfrozen n b s => freeSesSV s
          | FM x n s => freeSesSV s
   end.
 
@@ -123,7 +123,7 @@ Inductive subst_ses_sval : sval -> session -> sval -> sval -> Prop :=
    subst_ses_svt : forall x v, subst_ses_sval (SV x) x v v
    | subst_ses_svf : forall x y v, x <> y -> subst_ses_sval (SV y) x v v
    | subst_ses_fro : forall x v s b v', subst_ses_sval s x v v' -> subst_ses_sval (Frozen b s) x v (Frozen b v')
-   | subst_ses_unf : forall x v s b v', subst_ses_sval s x v v' -> subst_ses_sval (Unfrozen b s) x v (Unfrozen b v')
+   | subst_ses_unf : forall x v s n b v', subst_ses_sval s x v v' -> subst_ses_sval (Unfrozen n b s) x v (Unfrozen n b v')
    | subst_ses_fm : forall x v s y n v', subst_ses_sval s x v v' -> subst_ses_sval (FM y n s) x v (FM y n v').
 
 Inductive subst_ses_celem : cpred_elem -> session -> sval -> cpred_elem -> Prop :=
@@ -151,8 +151,8 @@ Inductive sval_check : atype -> aenv -> type_map -> sval -> Prop :=
   sval_check_sv: forall g env T s, ses_in s (dom T) -> sval_check g env T (SV s)
  | sval_check_frozen: forall g env T b s, sublist (freeVarsBexp b) env
              -> sval_check g env T s -> sval_check g env T (Frozen b s)
- | sval_check_unfrozen: forall g env T b s, sublist (freeVarsBexp b) env
-             -> sval_check g env T s -> sval_check g env T (Unfrozen b s)
+ | sval_check_unfrozen: forall g env T n b s, sublist (freeVarsBexp b) env
+             -> sval_check g env T s -> sval_check g env T (Unfrozen n b s)
  | sval_check_fm: forall g env T x n s, sval_check g env T s -> sval_check g env T (FM x n s).
 
 Inductive pred_check_elem : atype -> aenv -> type_map -> cpred_elem -> Prop :=
@@ -195,100 +195,22 @@ Axiom imply : cpred -> cpred -> Prop.
             triple q env T ([SEq (SV l) (Nval (C1) b)]) (AppSU (RH x)) ([SEq (SV l) (Hval (fun i => (update allfalse 0 (b i))))])
       | appu_pf : forall q env T l l1 m b e ba,  find_type T l (Some (l++l1,CH)) ->
                 eval_ch rmax env l m b e = Some ba ->
-                triple q env T ([SEq (SV (l++l1)) (Cval m b)]) (AppU l e) ([SEq (SV l) (Cval m ba)])
+                triple q env T ([SEq (SV (l++l1)) (Cval m b)]) (AppU l e) ([SEq (SV (l++l1)) (Cval m ba)])
+      | dis_pf : forall q env T x n l l1 n' m f m' acc, type_vari env x (QT n,l) -> find_type T l (Some (l++l1,CH)) ->
+                 ses_len l1 = Some n' -> dis_sem n n' m m f nil (m',acc) -> 
+                triple q env T ([SEq (SV (l++l1)) (Cval m f)]) (Diffuse x) ([SEq (SV (l++l1)) (Cval m' acc)])
+
       | for_pf : forall q env T x l h b p P i, l <= i < h ->
             triple q env T (cpred_subst_c P x i) (If (subst_bexp b x i) (subst_pexp p x i)) (cpred_subst_c P x (i+1)) ->
             triple q env T (cpred_subst_c P x l) (For x (Num l) (Num h) b p) (cpred_subst_c P x h)
       | if_c : forall q env T P Q a b e, class_bexp b = Some a ->
                  triple q env T (CP a::P) e Q -> triple q env T (CNeg a::P) PSKIP Q -> triple q env T P (If b e) Q
-      | if_q : forall q env T T' P P' Q Q' b e n l l1, type_bexp env b (QT n,l) -> find_type T l (Some (l++l1,CH)) ->
+      | if_q : forall q env T T' P P' Pa Q Q' Qa b e n l l1, type_bexp env b (QT n,l) -> find_type T l (Some (l++l1,CH)) ->
                   up_type T l1 CH T' -> subst_ses_cpred P (l++l1) (Frozen b (SV l1)) P' -> pred_check q env ([(l1,CH)]) Q' ->
-                  triple q env T' P' e (Q++Q') -> triple q env T P (If b e) Q.
-      | seq_pf: forall q env tenv tenv' tenv'' N N' N'' P R Q e1 e2,
-             @session_system qenv rmax q env tenv e1 tenv' -> up_types tenv tenv' tenv'' ->
-             triple q env tenv (N,P) e1 (N',R) -> triple q env tenv'' (N',R) e1 (N'',Q) -> 
-              triple q env tenv (N,P) (PSeq e1 e2) (N'',Q).
-
-Definition cpred_subst_c l x v := map (fun a => celem_subst_c a x v) l.
-
-Fixpoint subst_aexp_m (a:aexp) (x:var) (n:aexp) :=
-    match a with BA y => if x =? y then n else (BA y)
-              | Num a => (Num a)
-              | MNum r a => (MNum r a)
-              | APlus c d => match ((subst_aexp_m c x n),(subst_aexp_m d x n))
-                   with (Num q, Num t) =>  (Num (q+t))
-                      | (MNum r q, Num t) =>   (MNum r (q+t))
-                      | (Num t, MNum r q) =>   (MNum r (q+t))
-                      | (a,b) => APlus a b
-                             end
-              | AMult c d => match ((subst_aexp_m c x n),(subst_aexp_m d x n))
-                   with (Num q, Num t) =>   (Num (q*t))
-                      | (MNum r q, Num t) =>   (MNum r (q*t))
-                      | (Num t, MNum r q) =>   (MNum r (q*t))
-                      | (a,b) => AMult a b
-                             end
-              | Select s a => Select s (subst_aexp_m a x n)
-    end.
-
-Fixpoint sval_subst_m t x v :=
-  match t with ST a => ST a | SV s => SV s
-              | Mask s u z => Mask (sval_subst_m s x v) u (subst_aexp_m z x v)
-              | AppA a b => AppA a (sval_subst_m b x v)
-              | FSL e l s => FSL (sval_subst_m e x v) l s
-              | SSL e a b l1 l2 => SSL (sval_subst_m e x v) (sval_subst_m a x v) b l1 l2
-   end.
-
-Definition celem_subst_m t x v := 
-  match t with PFalse => PFalse 
-          | CBeq a b => CBeq (subst_aexp_m a x v) (subst_aexp_m b x v)
-          | CBge a b => CBge (subst_aexp_m a x v) (subst_aexp_m b x v)  
-          | CBlt a b => CBlt (subst_aexp_m a x v) (subst_aexp_m b x v)
-          | SEq a b => (SEq (sval_subst_m a x v) (sval_subst_m b x v)) end.
-
-Definition cpred_subst_m l x v := map (fun a => celem_subst_m a x v) l.
-
-Inductive sval_subst_q : sval -> session -> sval -> sval -> Prop :=
-    sval_subst_st: forall x l v, sval_subst_q (ST x) l v (ST x)
-  | sval_subst_sv_1: forall x l v, x = l -> sval_subst_q (SV x) l v v
-  | sval_subst_sv_2: forall x l v, x <> l -> sval_subst_q (SV x) l v (SV x)
-  | sval_subst_mask: forall y u z l v v', sval_subst_q y l v v' -> sval_subst_q (Mask y u z) l v (Mask v' u z)
-  | sval_subst_app: forall a b l v v', sval_subst_q b l v v' -> sval_subst_q (AppA a b) l v (AppA a v')
-  | sval_subst_fsl: forall a c b l v v', sval_subst_q a l v v' -> sval_subst_q (FSL a c b) l v (FSL v' c b)
-  | sval_subst_ssl: forall e a b l1 l2 l v v1 v2, sval_subst_q e l v v1 -> sval_subst_q a l v v2
-                                -> sval_subst_q (SSL e a b l1 l2) l v (SSL v1 v2 b l1 l2). 
-
-Inductive celem_subst_q : cpred_elem -> session -> sval -> cpred_elem -> Prop :=
-    celem_false: forall l v, celem_subst_q (PFalse) l v (PFalse)
-  | celem_beq: forall l v x y, celem_subst_q (CBeq x y) l v (CBeq x y)
-  | celem_bge: forall l v x y, celem_subst_q (CBge x y) l v (CBge x y)
-  | celem_blt: forall l v x y, celem_subst_q (CBlt x y) l v (CBlt x y)
-  | celem_seq: forall l v x x' y y', sval_subst_q x l v x' -> 
-          sval_subst_q y l v y' -> celem_subst_q (SEq x y) l v (SEq x' y').
-
-Inductive cpred_subst_q : cpred -> session -> sval -> cpred -> Prop :=
-   cpred_empy: forall l v, cpred_subst_q nil l v nil
- | cpred_many: forall p p' P P' l v, celem_subst_q p l v p' -> cpred_subst_q P l v P' ->
-                 cpred_subst_q (p::P) l v (p'::P').
-
-Inductive state_syn_val : list session -> sval -> Prop :=
-  | state_syn_st : forall l x, state_syn_val l (ST x)
-  | state_syn_sv : forall l x, In x l -> state_syn_val l (SV x)
-  | state_syn_mask : forall l y u z, state_syn_val l y -> state_syn_val l (Mask y u z).
-
-Inductive state_syn_elem : list session -> cpred_elem -> Prop :=
-    state_pfalse : forall l, state_syn_elem l (PFalse)
- |  state_cbeq : forall l x y, state_syn_elem l (CBeq x y)
- |  state_cblt : forall l x y, state_syn_elem l (CBlt x y)
- |  state_seq : forall l x y, state_syn_val l x -> state_syn_val l y -> state_syn_elem l (SEq x y).
-
-Definition state_syn (l:type_map) (t:cpred) := Forall (fun x => state_syn_elem (dom l) x) t.
-
-
-(*TODO: define the model function of a state, and claim that the type and model function of a state should match. *)
-Section Triple. 
-
-
-
-
-End Triple.
+                subst_ses_cpred P (l++l1) (Unfrozen n (BNeg b) (SV (l++l1))) Pa ->
+                subst_ses_cpred Q' l1 (Unfrozen n b (SV (l++l1))) Qa ->
+                  triple q env T' P' e (Q++Q') -> triple q env T P (If b e) (Pa++Qa)
+      | seq_pf: forall q env tenv tenv' tenv'' P R Q e1 e2,
+             @session_system rmax q env tenv e1 tenv' -> up_types tenv tenv' tenv'' -> pred_check q env tenv'' R ->
+             triple q env tenv P e1 R -> triple q env tenv'' R e1 Q -> triple q env tenv P (PSeq e1 e2) Q.
 
